b3e275b6839ce78b59cd43209fbc47b1b22febb6
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:27:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
 def oxford_comma(array)
-
+  return array.first if array.size == 1
+  return array.join(" and ") if array.size == 2
+  "#{(array.first array.size - 1).join(', ')}, and #{array.last}"
 end
