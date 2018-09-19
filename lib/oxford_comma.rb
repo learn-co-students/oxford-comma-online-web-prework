5e3ab35b02a684c6ac64d87e056e54a1fceec7f3
Harry Bloch <harryfbloch@gmail.com>
2018-09-19 23:10:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  newString = ""
+  if array.length > 2
+    array.each_with_index {|element, index| 
+      if (index != array.length - 1)
+        newString.concat("#{element}, ")
+      else
+        newString.concat("and #{element}")
+      end
+    }
+  elsif array.length == 2
+    newString = "#{array[0]} and #{array[1]}"
+  else
+    newString = array[0]
+  end
+  return newString
 end
+
+
+
