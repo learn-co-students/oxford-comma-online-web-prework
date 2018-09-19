bc974e089466284e0daa348517ad62e3cd00f30a
Matthew McCormack <mmccormack1182@gmail.com>
2018-09-19 13:12:02 -0400

    Tests Passed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join( " and " )
+  else
+    last_element = array[array.length - 1]
+    array[array.length - 1] = "and #{last_element}"
+    array.join( ", " )
+  end
 end
