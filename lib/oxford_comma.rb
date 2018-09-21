c9cc7480e82bbc3399e898cffbed207476385fe7
Anuj Panchal <anujpan1@gmail.com>
2018-09-21 16:17:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    joined = array.join
+  elsif array.length == 2
+    joined = array.join(" and ")
+  else
+    last = array.pop()
+    last = "and " + last
+    array.push(last)
+    joined = array.join(", ")
+  end
 end
