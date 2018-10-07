7c87f8355e647670aae7ed215ec0f60dc21537db
Brandon Teich <brandonteich123@gmail.com>
2018-10-07 03:37:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
 if array.length == 1
-  array[0]
+    array[0]
 elsif array.length == 2
-  array[-2] << " and "
-  array.join
+    array[-2] << " and "
+    array.join
   elsif array.length  > 2
     array[-1].prepend "and "
 	  array.join(", ")
