292411b31c2365f768287ab44c9e210c42bc328a
Brandon Teich <brandonteich123@gmail.com>
2018-10-07 03:28:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,12 @@ def oxford_comma(array)
   newarray = []
   if array.length == 2
      array[-2] << " and "
-     array.join
+    newarray  array.join
   elsif array.length == 1
-  	  array.join
+  	  newarray array.join
   elsif array.length > 2
       array[-1].prepend "and "
 	    newarray = array.join(", ")
   end
+  newarray
 end
