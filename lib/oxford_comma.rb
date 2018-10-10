77b02faeeecec118b7940258da207102fe28db6c
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-10 23:40:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    return array[0] 
+  end 
+  array[array.size-1] = "and #{array[array.size-1]}"
+  if array.size > 2
+    str = array.join(", ")
+  else 
+    str = array.join(" ")
+  end 
+  return str
 end
