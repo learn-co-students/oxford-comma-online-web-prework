5b7bedcb7acecf3060fb7d40184e201fda8810bc
grant smith <grantsmith1256@gmail.com>
2018-10-13 21:02:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  if array.length === 1 
-    return array[0]
+  if array.size == 1
+    return "#{array[0]}"
   elsif array.size == 2
-    return "#{array[0]} and #{array[1]}"
-  end
-  
-  first = array[0...array.length-1].join(", ")
-  "#{ first }, and #{ array[array.length-1] }"
+  return "#{array[0]} and #{array[1]}"
+end
+
+return "#{array[0...array.length-1].join(", ")}, and #{array[-1]}"
 end
