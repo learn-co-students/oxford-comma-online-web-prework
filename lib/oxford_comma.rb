0ad244f0c44eaea03e4e46629203f4f74e4bfb36
Jane Venter <janeanneventer@gmail.com>
2018-10-15 04:57:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    array.join(" and ")
+  elsif array.length > 2
+    last = array.pop
+    list = array.join(", ")
+    "#{list}, and #{last}"
+  else
+    array.join
+  end
 end
