b6186fc063ad6f3da0d38f46b5e552f33d804c09
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-15 02:05:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  #puts array[0,array.length-1]
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  end
+  listString=array[0,array.length-1].join(", ")+", and #{array.last}"
 
 end
+
+#puts oxford_comma(["Alex","emily","Nate"])
+#puts oxford_comma(["Alex","emily"])
+#
