4cca63cf623edc7b0a6a72c8af44a018b1c9ee5a
Eric Mayfield <eric@visiblycreative.com>
2018-10-18 16:46:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+  #determine array size
+  array_size = array.size
+  if(array_size == 1)
+    array.join
+  elsif(array_size == 2)
+    array.join(" and ")
+  elsif(array_size > 2)
+    new_str = ""
+    while(array_size > 0)
+      if(array_size > 1)
+        new_str << array.shift
+        new_str << ", "
+        array_size -= 1
+      else
+        new_str << "and "
+        new_str << array.shift
+        array_size -= 1
+      end
+    end
+    new_str
+  end
 end
