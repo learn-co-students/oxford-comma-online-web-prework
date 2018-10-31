23b01c760e6e44cdaf880837061fa6a0aa03c94b
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-31 03:59:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size == 1
+      array.join
+    elsif array.size == 2
+     array.join(" and ")
+    elsif array.size == 3
+      index = array[-1].length
+      array.join(", ").insert(-index - 1, "and ")
+    else
+      index = array[-1].length
+      array.join(", ").insert(-index - 1, "and ")
+  end
 end
