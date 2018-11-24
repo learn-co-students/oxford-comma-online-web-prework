3146e96c306484cc58c10fe7eb9c0e0ae5714db0
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 20:51:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    array[-2] << ", and"
+    array.join(", ")
+    binding.pry
+  elsif array.length > 2
+    array[-2] << ", and"
+    array.join(", ")
+    binding.pry
+  else
+    array.join()
+    binding.pry
+  end
 end
+
