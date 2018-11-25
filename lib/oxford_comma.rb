cd233a4caa33bc010a07722db4f46d664eb57f44
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-25 16:50:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
-def oxford_comma(array)
-  if array.length == 2
-    array[-2] << ", and"
-    array.join(", ")
-    binding.pry
-  elsif array.length > 2
-    array[-2] << ", and"
-    array.join(", ")
-    binding.pry
-  else
-    array.join()
-    binding.pry
-  end
-end
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    array[-1].prepend "and "
+    array.join(", ")
+end
+end
