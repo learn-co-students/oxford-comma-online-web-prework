cae04f68712ffc45d1b2687f732a20b7c7ff4457
Robert McCary <robert.mccary@yahoo.com>
2018-11-30 18:09:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2
+    array[-1].insert(0, "and ")
     array.join(", ")
   else
     array.join
   end
-end
 end
