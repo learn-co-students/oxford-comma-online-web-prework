87c2c7dd917878ca575b43b2e5693eae25eb89c4
Victoria B <viktorija.bourassa@gmail.com>
2018-12-13 21:21:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.count == 1
+return array.join
+elsif array.count == 2
+    array.insert(1, "and")
+    return array.join(" ")
+return array
+else
+  string = array.join(",")
+  arr = string.split(",")
+  arr[-1] = "and " + arr[-1]
+  string2 = arr.join(", ")
+  end
 end
