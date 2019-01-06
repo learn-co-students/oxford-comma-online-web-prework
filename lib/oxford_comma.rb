fc4501420ca8814c2449b38c4b2fb03a713a3d68
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 02:10:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length > 1
+      array[-1] = "and #{array[-1]}"
+    end
+    if array.length > 2
+      return array.join(", ")
+    else
+     return array.join(" ")
+    end
 end
