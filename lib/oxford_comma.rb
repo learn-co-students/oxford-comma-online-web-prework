e6f927ebe2f434296dda2d41ffdbd7f595a5df5a
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-11 21:30:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+  if array.count == 1
+    
+    array.join
+
+  elsif array.count == 2
+    
+      array.join(" and ")
+      
+  else
+   
+     last = array.pop
+     narr = array.join(', ')
+     narr << ", and "
+     narr << last
+     
+  end
+
 end
