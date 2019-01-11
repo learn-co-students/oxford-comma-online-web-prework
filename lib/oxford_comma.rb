1beaeda8af3c4289bf7bebce4c1286dff072daa8
Eyal Toledano <eyaltoledano@me.com>
2019-01-11 05:08:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    part1 = array.slice(0,2).join(", ")
+    part2 = array.slice(2)
+    full = "#{part1}, and #{part2}"
+  else array.size > 3
+    part2 = array.pop
+    part1 = array.join(", ")
+    full = "#{part1}, and #{part2}"
+  end
 
 end
