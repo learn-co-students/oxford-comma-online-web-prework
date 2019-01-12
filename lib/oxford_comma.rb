b3914677db28ea0afa36952bff1264263ffd8483
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-12 05:45:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(kiwi)
+"kiwi"
 end
+def oxford_comma("kiwi", "durian")
+  "kiwi and durian"
+end
+def oxford_comma("kiwi", "durian", "starfruit")
+  "kiwi, durian, and starfruit"
+end
+def oxford_comma("kiwi", "durian", "starfruit", "mangos", "dragon fruits")
+  "kiwi, durian, starfruit, mangos, and dragon fruits"
