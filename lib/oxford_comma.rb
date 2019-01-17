283fc5903dae62185432d70d0f01d6c6ae9c760c
Vinh Do <vinhdo07@gmail.com>
2019-01-17 01:56:30 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return "#{array.first}"
+  elsif array.size == 2
+    return "#{array.join(" and ")}"
+  else
+    last_ele = array.pop
+    return "#{array.join(", ")}, and #{last_ele}"
+  end
 end
