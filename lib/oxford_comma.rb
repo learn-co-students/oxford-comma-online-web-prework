2951a2a46be9b19cc4b6f0b3eeced601735a5c56
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-01-21 05:55:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.count == 1 
+    arr[0]
+  elsif arr.count == 2 
+    arr.join(" and ")
+  else
+    last_position = -1 * (arr[-1].length + 1)
+    new_string = arr.join(", ")
+    new_string.insert(last_position, "and ")
+  end
 end
