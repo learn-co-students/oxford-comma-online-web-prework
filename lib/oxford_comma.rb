cdcee3bd439e1ea696a4011e8fc678f27b46a615
Lu Daley <daleyluis@gmail.com>
2019-01-24 02:04:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
   array.join
 elsif array.length == 2 
   array.join(" and ")
-else array.compact.join(", ").insert.last(" and")
+else array.compact.join(", ").insert(-11," and")
      #array.insert(-2,"and")
 end
 end 
