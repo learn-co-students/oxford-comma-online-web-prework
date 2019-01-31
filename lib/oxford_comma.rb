95eedea7dec9f2486b5f38acfc50766bc302ba8e
Joan in DC <joanrigdon@gmail.com>
2019-01-31 18:47:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,34 @@
+require 'pry'
+
 def oxford_comma(array)
-  
-  if 
-    array.length == 1 
-    array.shift
-  elsif 
-    array.length == 2   
-    array.join (" and ")
-  elsif 
-    array.length > 2
-      last = array.pop
-      string = array.join(", ")
-      return string + ", and " + last
+  if array.length == 1 
+    return array.shift
   end
   
+  if array.length == 2 
+    return array.join(" and ")
+  end
+  
+  if array.length > 2
+    last = array.pop
+    return array.join(", ") + ", and #{last}"
+    binding.pry
+  end
 end
+
+# def oxford_comma(array)
+  
+#   if 
+#     array.length == 1 
+#     array.shift
+#   elsif 
+#     array.length == 2   
+#     array.join (" and ")
+#   elsif 
+#     array.length > 2
+#       last = array.pop
+#       string = array.join(", ")
+#       return string + ", and " + last
+#   end
+  
+# end
