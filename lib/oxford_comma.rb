edfd4578b76938993433945126f38df6b0e15f2b
lieby weintraub <liebahinda@gmail.com>
2019-01-31 18:28:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.size == 1
+  array.join
+elsif array.size == 2 
+array.join("and")
+else final = array.last
+    array.pop
+    string = array.join(", ")
+    string << ", and #{final}"
 end
