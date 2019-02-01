88036a284021a77675006c7c04f7101a6f5ef480
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-02-01 20:20:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+#binding.pry
+    new_string = array.join(", ")
+    #new_string.delete[]
+    #new_string.insert(-2, and ")
+#Need to: find:
+# -- a way to join "and" to the string
+# -- at the right position inside
+#Position for 'and' in string: array.insert(array.length - 1, " and ") => placement of "and"
+  end
 end
