153ffd83d5f6219ccedb20739efdcb228a7d1155
Chance Smith <chancesmith97@hotmail.com>
2019-02-04 19:38:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    pos = 2
+    [array[0...pos], array[pos..-1]].map { |x| x.join ', ' }.join(', and ')
+  else array.length > 3
+    pos = array.size - 1
+    [array[0...pos], array[pos..-1]].map { |x| x.join ', ' }.join(', and ')
+
+
+  end
 
 end
