c00926b9f28cf32dcb2db7105ec768ab5de13e73
Brandi Smith <brandi@Brandis-MBP.jetpack>
2019-02-05 12:53:07 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        format_one(array)
+    elsif array.size == 2
+        format_two(array)
+    else
+        format_three(array)
+    end
 end
+
+def format_one(array)
+    array.join
+end
+
+def format_two(array)
+    array.join(' and ')
+end
+
+def format_three(array)
+    last = array.pop
+    array.join(', ') + ", and #{last}"
+end
+
