0306ccb63a1aefee393e45ceede7097d6f811007
Jay Sokolowski <jaysoko@gmail.com>
2019-02-06 23:37:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1
+      array[0]
+    when 2
+      "#{array.first} and #{array.last}"
+    when 3
+      "#{array.first}, #{array[1]}, and #{array.last}"
+    else
+      fruit_length = array.length
+      last_element = array.pop
+      fruit_array = array.join(', ')
+      "#{fruit_array}, and #{last_element}"
+end
 end
