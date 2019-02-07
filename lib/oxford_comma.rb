a1e7417e7f7e345d8d43e3dd667fc368e5701063
Katie Blizzard <kblizzard11@gmail.com>
2019-02-07 02:45:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
 
+  case array.size 
+    when 1 
+      return array[0]
+    when 2 
+      return array.join(" and ")
+    when 3 
+      return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  end 
+  
+  if array.size > 3
+     message = ""
+     for x in array
+      while array.index(x) < array.size - 1 
+        message << "#{x}, "
+      end
+      if array.index(x) == array.size - 1 
+        message << "and #{x}"
+      end
+   end
+  end
+
 end
