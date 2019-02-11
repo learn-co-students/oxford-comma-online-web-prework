e6e19bb8bf0b131e6a8c8a2b9f2a8faf2a04f5a2
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-11 22:16:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  return array.join if array.length == 1
+  
+  return array.join(" and ") if array.length == 2
+  
+  if array.length >= 2
+    array[-1].prepend("and ")
+    array.join(", ")
+  end
 end
