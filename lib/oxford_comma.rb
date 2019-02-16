44635d07dfc5d33bdc411b97ee11c7c813e15519
kyle smith <kylemclellan@gmail.com>
2019-02-16 20:00:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.first 
+  elsif array.length == 2 
+    array.join(" and ")
+  else 
+    array << "and " + array.pop
+    array.join(", ")
+  end
 end
