7c829512d039b3bd71a1e645a62080be1f8a0a55
David Stewart <davidstewart@gmail.com>
2019-02-19 05:14:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
+require 'pry'
+name = ["Bill","Ron","Steeve"]
+name2 = ["David","Rickie"]
+
 def oxford_comma(array)
+if array.length == 2
+  array.join(" and ")
+  
+elsif array.length == 1
+return array[0]
+
+else 
+  last = array.pop
+subarray = array
+values = array.join(", ")
+return values << ", and #{last}"
+
+ 
 
 end
+
+end
+#binding.pry
