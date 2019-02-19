11446a3747aec25751e9d635c10b4f0f3224e3b2
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-02-19 23:23:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_array = array.slice(0, array.length - 1)
+    return "#{new_array.join(", ")}, and #{array[array.length - 1]}"
+  end
 end
