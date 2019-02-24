57104079af1ba8abd114c99ed20b4f4482856a73
Samuel W Tong <ssamt1142@gmail.com>
2019-02-24 23:44:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
-end
+  if array.count == 1
+    array*","
+  elsif array.count == 2
+    array*" and "
+  else
+    last = array.pop
+ last_fruit = ", and " << last
+ front_fruits = array.join(", ")
+ front_fruits << last_fruit
+ end
+ end
