c9fb3c4a48997f2c03e38051b2acfa0cf24d5897
Daniel Schneider <daschne8@gmail.com>
2019-02-25 17:27:28 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  case array.length
+  when 1
+    return array.join
+  when 2
+    return array.join(" and ")
+  when 3
+    return array[0,2].join(", ") + ", and " + array[2]
+  else
+    len = array.length
+    return array[0,len-1].join(", ") + ", and " + array[len-1]
+  end
 
 end
