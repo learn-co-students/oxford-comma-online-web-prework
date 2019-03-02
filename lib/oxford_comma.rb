2a61b7822d75336070fb3db4917360fbfc19b546
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 08:39:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array << "and #{array[-1]}"
+    array.delete_at(-2)
+    return array.join(", ")
+  end
 end
