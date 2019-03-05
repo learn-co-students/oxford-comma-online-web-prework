39214b8ac32e2d6a9e8d8cc660de5a7a30af24ef
chandra mohan <mohaniyer_78@hotmail.com>
2019-03-05 06:55:21 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem "pry"
+
+
+
+
+
--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  if array.length == 1 
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    sentence = ""
+    array.each_with_index do |word , index|
+      # sentence == "kiwi, durian, strawberry"
+      if index == array.length - 1 
+        #sentence += ", and "
+        sentence = sentence + ", and "
+        #sentence == "kiwi, durian, strawberry, and "
+      elsif index != 0
+        sentence += ", "
+      end
+      sentence += word
+      # sentence == "kiwi, durian, strawberry, and banana"
+    end
+    sentence
+  end
 end
+
