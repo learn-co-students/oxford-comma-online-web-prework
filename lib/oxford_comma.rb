99b1423fad9423db3f4669457177174761079990
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-05 19:33:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_array = array[0...-1]
+    string = ""
+    new_array.each do |item|
+      string << item + ", "
+    end
+    string << "and " + array[-1]
+  end
 end
