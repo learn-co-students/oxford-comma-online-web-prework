b80a858e0a5926f2eca5035c9fa0730916d73f9f
Eric Storey <estorey@gmail.com>
2019-03-08 22:39:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  str=""
+  if array.length == 1 
+    str << array.shift
+  elsif array.length == 2 
+    str << array.shift
+    str << " and "
+    str << array.shift
+  else
+    array.each_with_index do |word, i|
+      if i + 1 < array.length 
+        str << "#{word}, "
+      else 
+        str << "and #{word}"
+      end
+    end
+  end
+  str
 end
