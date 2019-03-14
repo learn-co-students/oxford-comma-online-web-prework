15439da21f2ef6907313ad48cf09768f3b47497a
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-14 01:25:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join(" ")
+  elsif array.length == 2
+    last_element = array.pop.split(' ')
+    string2 = last_element.unshift(" and").join(" ")
+    string1 = array.join(", ")
+    final_string = string1 + string2
+  else
+    last_element = array.pop.split(' ')
+    string2 = last_element.unshift(", and").join(" ")
+    string1 = array.join(", ")
+    final_string = string1 + string2
+  end
 end
