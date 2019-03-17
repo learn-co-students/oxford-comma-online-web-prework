c54762c2ca838a6f2f5ea9ab9b2ac3a570273417
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 05:06:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  last_item = array.last
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+  array.pop
+  array.join(", ")
+  return "#{array.join(", ")}, and #{last_item}"
+  end
 end
