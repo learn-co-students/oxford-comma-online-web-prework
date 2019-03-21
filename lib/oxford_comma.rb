0a45e37ab17fbb03d751459676b8738b6ee93c8a
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-21 00:30:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(" and ")
+  elsif array.length >= 3
+    new_string = array[0..-2].join(", ") + ", and " + array[-1]
+    return new_string
+  end
 end
