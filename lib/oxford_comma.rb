0212c80e3293ebfedf666cab9b00743de5cdd3b3
Denali Balser <denali.balser167@gmail.com>
2019-03-25 17:30:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require "pry"
 def oxford_comma(array)
-
+  if array.length >= 3
+    array.last.insert(0, "and ")
+    #array.insert(-2, "and")
+  elsif array.length == 2
+    array.insert(1, " and ")
+    return array.join
+  else
+    array
+ end
+ array.join(", ")
 end
