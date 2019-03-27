074c7079820abf57933101deb6a9b6bc58181dd3
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-27 02:07:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
-end
+  size = array.size 
+  if  size == 1 
+  array.join(" ")
+  elsif size == 2 
+  array.join(" and ")
+  elsif size >= 3
+  last_item = array.last
+  array.delete_at(size-1)
+  string = array.join(", ")
+  string << ", and #{last_item}"
+ # elsif size > 3
+  
+  end #end if
+end #end oxford_comma()
