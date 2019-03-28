19a34a1615531a54ea0610e344ddd36cbaaf1616
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-28 15:09:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
-
+def oxford_comma(array) 
+  oxford_comma= Array.new
+  oxford_comma return Array if Array= Array 
+  
+  elsif 
+  Array.size=2.include "," 
+  oxford_comma * ","
+  array.insert(1, "and").join(" ")
 end
+elsif
+array.size=3.include "," starfruit."and"
+end
+
+elsif
+Array.size>3
+array.insert(1,"and").join(" ")
+end 
+end 
