4173e115e01f8374e2ebc87ff09c77b6c2ecc2a1
Ryan Humphries <r.humphries2@gmail.com>
2019-04-11 17:49:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
 
+
+if array.length <= 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.length >= 3
+  lastitem = array.pop
+  newstring = array.join(", ")
+  return "#{newstring}, and #{lastitem}"
+else
+  "Error"
 end
+
+end
+
+#  lastitem = " and #{array[-1]}"
