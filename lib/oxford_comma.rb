dab7e314126854d893c9d9356b28ba60f6ea7830
Jessica Hong <jessicahong.24@gmail.com>
2019-04-14 12:58:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.length < 2
+  array.join
+  elsif array.length == 2
+  array.join (" and ")
+  else 
+   array [0...-1].join(", ") + ", and " + array [-1]
+  end
+ end
