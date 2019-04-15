a976cb57120b47532faf0218eedacd5f33478623
Garth Lilly <garff247@gmail.com>
2019-04-15 03:42:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  x = array.length
+  string = String.new
+  if x < 3
+  string = array.join(" and ")
+  else
+  new_string = "and " + array.last
+  #string = array.join(", and ")
+  array[-1] = new_string
 
+  string = array.join(", ")
+end
+string
 end
