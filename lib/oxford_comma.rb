cdaa8410e5730997774d9bc8cc9461ca8956f08e
Jess Rezac <jess.rezac@gmail.com>
2019-04-23 22:57:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    last_item = array.last
+    array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{last_item}"
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    array.join
+  end
 end
