0a237042aaa5ce17a74884624d177ed893744e57
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-24 17:43:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length === 1   
+    array.join 
+  elsif array.length === 2
+    array.join(" and ")     
+  elsif array.length === 3
+    newArray = "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else 
+    lastArray = array.last
+    array.pop
+    array << "and #{lastArray}"
+    array.join(", ") 
+    
+  end    
 end
