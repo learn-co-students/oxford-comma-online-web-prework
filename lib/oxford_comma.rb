c7e92922811123f2f9bf87fc07f132eda2017b58
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-07 20:11:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.join
+  elsif array.length == 2
+  return array.join(" and ")
+  else
+    final = array.last
+    array.pop
+    craze = array.join(", ")
+    return (craze + ", and #{final}")
+  end
 end
