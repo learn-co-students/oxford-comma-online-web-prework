63cfaab34cbec92b6fd1ba0c7cf65ed4a1ee5584
Angela Lo <angelawenlo@gmail.com>
2019-05-15 21:06:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
-def oxford_comma(array)
-  if array.length < 2
-    array.join(",")
-  elsif array.length == 2
-    array.join(" and ")
-  else 
-    array[-1].insert(0,"and ")
-    array.join(", ") 
-  end
+def oxford_comma(fruits)
+  if fruits.length == 1
+   fruits.join
+ elsif fruits.length == 2
+   fruits.join(" and ")
+ elsif fruits.length == 3
+  fruits[-1].prepend("and ")
+  fruits.join(", ")
+else
+ fruits[-1].prepend("and ")
+ fruits.join(", ")
+ end
 end
