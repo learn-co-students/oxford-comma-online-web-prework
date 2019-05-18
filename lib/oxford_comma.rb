5d3c4c051777c21f7dd81b5788cc905ae7fff6f4
Jack Atherton <jackrrye@gmail.com>
2019-05-18 17:20:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  array2 = []
+  if array.size == 1 
+    array.join 
+  elsif array.size ==2 
+    array = array.insert(1, " and ")
+    array.join
+  else
+    size = array.size 
+    last = array.pop
+    array = array.join(", ")
+    last = last.prepend(", and ")
+    array << last
+  end
 end
