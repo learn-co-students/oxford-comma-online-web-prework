06b26037b212348e29e9f368252a2e61bb4948ed
Justine Berretta <justberretta@gmail.com>
2019-05-23 03:32:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.length == 2 
+    "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    "#{array[0..-2].join(", ")}, and #{array.last}" 
+  else
+    array.join
+  end 
+end 
