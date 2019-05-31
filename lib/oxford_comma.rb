3365b8754e9a6e3393177e9dc0a6910e0baf6008
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-31 02:02:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+  if array.count == 1 
+    return array.join
+  elsif array.count == 2 
+    return array.join(" and ")
+  else
+    lastStr = array.pop
+    array.join(", ") << ", and #{lastStr}"
+  end
+end 
