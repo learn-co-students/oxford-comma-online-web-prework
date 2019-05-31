0caeea13ee210a9e8e2c436cc44371405b20956d
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-31 12:26:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ def oxford_comma(array)
   elsif array.size==2
     array.join(" and ")
   else array.size>=3
-    last = array.pop
-    array.insert((-1), "and  ")
-    array = array.join(", " + "  ")
-    nuarray = array.split
-    nuarray.join(" ")+ " " + last
+    last = array.pop#popped off last part of array and saved in last.
+    array.insert((-1), "and  ")#insert "and" onto end of array.
+    array = array.join(", " + "  ")#array.join creates a string and seperates with commas.
+    nuarray = array.split#split string to create a new array
+    nuarray.join(" ")+ " " + last#join new array to return a string while adding popped off portion from beginning.
 end
 end
