00e9434099df213335b773e50e2f053ac214cfce
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-05-31 12:21:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size==1
+    return array.join
+  elsif array.size==2
+    array.join(" and ")
+  else array.size>=3
+    last = array.pop
+    array.insert((-1), "and  ")
+    array = array.join(", " + "  ")
+    nuarray = array.split
+    nuarray.join(" ")+ " " + last
+end
 end
