6ba8e8ca1567efa5f8564f2afbcd72277444f851
Amy Westcott <amy.westcott@yahoo.com>
2019-06-07 20:37:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def oxford_comma(array)
+  i = 0
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+  array.join(" and ")
+else 
+ string = array.join(", ")
+ word = array.last
+ string = string.chomp(word)
+ string << "and "
+ string << word
+
+end
 
 end
