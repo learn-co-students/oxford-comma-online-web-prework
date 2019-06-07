2c7c16e0eb04382a271ac04c2171b997cd6d0232
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 12:45:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array = array.join("")
+    array.insert(-7, "and")
+  elsif array.length == 3 
+    array = array.join(",")
+    array.insert(-10, "and")
+  elsif array.length == 5 
+    array = array.join(",")
+    array.insert(-14, "and") 
 end
